7e469a6feed04f5d71685649ad4566e5d3a8c9e2
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-11 20:03:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 const makeChange = (c) => {
   // your name here
+	let amount=c/100;
+	let PennyIndollar=0.01;
+	let NickleIndollar=0.05;
+	let DimeIndollar=0.10;
+	let QuarterIndollar=0.25;
+	let Quarter=0;
+	let Penny=0;
+	let Nickle=0;
+	let Dime=0;
+	while(amount>=QuarterIndollarIndollar){
+       Quarter++;
+		amount-=QuarterIndollar;
+	}
+	while(amount>= DimeIndollar){
+	Dime++;
+		amount-=DimeIndollar;
+	}
+	while(amount>= NickleIndollar){
+	Nickle++;
+		amount-=NickleIndollar;
+	}
+	while(amount>= PennyIndollar){
+	Penny++;
+		amount-=PennyIndollar;
+	}
+	let res={
+		q:Quarter,
+		d:Dime,
+		n:Nickle,
+		p:Penny
+	};
+	return res;
 };
 
 // Do not the change the code below
